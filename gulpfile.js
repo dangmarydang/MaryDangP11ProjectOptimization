0d68789046f6d3003d4e9aa280718745068dcf54
Mary Dang <maryvdang@gmail.com>
2016-08-15 00:25:59 -0700

    Initial commit

---
+++ gulpfile.js
@@ -0,0 +1,37 @@
+"use strict";
+
+var gulp = require('gulp'),
+  concat = require('gulp-concat'),
+  uglify = require('gulp-uglify'),
+  rename = require('gulp-rename'),
+  sass = require('gulp-sass'),
+  maps = require('gulp-sourcemaps');
+
+gulp.task("concatScripts", function() {
+  gulp.src([
+    'js/jquery.js',
+    'js/fastclick.js',
+    'js/foundation.equalizer.js',
+    'js/foundation.js',
+    'js/foundation.reveal.js',
+    'js/scripts.js'])
+  .pipe(maps.init())
+  .pipe(concat("app.js"))
+  .pipe(maps.write('./'))
+  .pipe(gulp.dest("js"));
+});
+
+gulp.task("minifyScripts", function() {
+  gulp.src("js/app.js")
+    .pipe(uglify())
+    .pipe(rename('app.min.js'))
+    .pipe(gulp.dest('js'));
+});
+
+gulp.task("compileSass", function() {
+  gulp.src("scss/application.scss")
+    .pipe(maps.init())
+    .pipe(sass())
+    .pipe(maps.write('./'))
+    .pipe(gulp.dest("css"));
+});
